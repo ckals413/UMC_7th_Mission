e77df4878e017d9bc55b590582dc6f1d85c46499
Chamin Im <ckals413@naver.com>
2024-11-15 02:28:59 +0900

    FEAT: 멤버조회 인터페이스, 구현체

---
+++ src/main/java/umc/spring/repository/MemberRepository/MemberRepositoryCustom.java
@@ -0,0 +1,6 @@
+package umc.spring.repository.MemberRepository;
+
+//커스텀 메서드의 선언을 위한 인터페이스
+//현재 메서드가 정의되어 있지 않지만, 필요 시 여기에 쿼리 메서드를 선언할 수 있다
+public interface MemberRepositoryCustom {
+}
---
+++ src/main/java/umc/spring/repository/MemberRepository/MemberRepositoryImpl.java
@@ -0,0 +1,5 @@
+package umc.spring.repository.MemberRepository;
+
+//MemberRepositoryCustom의 구현체로, 실제 로직이 필요하다면 이 클래스에 추가할 수 있다
+public class MemberRepositoryImpl implements MemberRepositoryCustom {
+}
